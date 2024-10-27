754766bd211d3dd0448ccfd1372a94d200b7e0e3
milche <milosz.chelpa@gmail.com>
2024-10-27 23:24:32 +0100

    damaged to h5

--- dataset/generating_damage_h5.py
+++ dataset/generating_damage_h5.py
@@ -1,27 +1,41 @@
 import h5py
 import numpy as np
+from PIL import Image, ImageDraw
 from datasets import load_from_disk
 from time import time
+
+def generate_square_damage(image: Image.Image, square_side=64) -> Image.Image:
+    image = image.copy()  # Tworzymy kopię obrazu
+    draw = ImageDraw.Draw(image)
+
+    # Losowa pozycja kwadratu
+    x = np.random.randint(0, image.size[0] - square_side)
+    y = np.random.randint(0, image.size[1] - square_side)
+    square_coords = [(x, y), (x + square_side, y + square_side)]
+
+    # Rysowanie białego kwadratu
+    draw.rectangle(square_coords, fill=(255, 255, 255))
+    
+    return image
 
 if __name__ == '__main__':
     # Załaduj dane z dysku
     ds = load_from_disk(r'C:\vscodeProjects\WikiArt_Inpainting\WikiArt_damaged_100')
     
     start = time()
-    # Zakładam, że twoja kolumna z obrazami nazywa się 'image'
     images = ds['image']
 
-    # Przetwarzanie obrazów: konwersja na numpy array i skalowanie do uint8
-    image_arrays = [np.array(img.convert("RGB"), dtype=np.uint8) for img in images]  # Konwersja obrazów na numpy i uint8
+    # Przetwarzanie obrazów: nakładanie uszkodzenia, konwersja na numpy i skalowanie do uint8
+    image_arrays = [np.array(generate_square_damage(img).convert("RGB"), dtype=np.uint8) for img in images]
 
     # Połącz listę numpy array w jedną tablicę (B x H x W x C) i zamień oś na (B x C x H x W)
     image_array_stack = np.stack(image_arrays).transpose(0, 3, 1, 2)
 
     # Zapisz dane numpy do pliku .h5 jako uint8
-    with h5py.File('images_tensor_data.h5', 'w') as h5f:
+    with h5py.File('damaged_images_tensor_data.h5', 'w') as h5f:
         h5f.create_dataset('image', data=image_array_stack, compression='gzip', compression_opts=9, dtype='uint8')
 
     end = time()
 
     print(f'{end - start} seconds')
-    print("Obrazy zostały zapisane do pliku images_tensor_data.h5 jako uint8")
+    print("Obrazy z uszkodzeniami zostały zapisane do pliku damaged_images_tensor_data.h5 jako uint8")
