91de4026b66cc10959e33d15ac63adeec2c6986e
milche <milosz.chelpa@gmail.com>
2024-10-28 02:17:32 +0100

    h5 done

--- dataset/generating_damage_h5.py
+++ dataset/generating_damage_h5.py
@@ -3,39 +3,52 @@ import numpy as np
 from PIL import Image, ImageDraw
 from datasets import load_from_disk
 from time import time
+from tqdm import tqdm
 
 def generate_square_damage(image: Image.Image, square_side=64) -> Image.Image:
-    image = image.copy()  # Tworzymy kopię obrazu
+    image = image.copy()
     draw = ImageDraw.Draw(image)
 
-    # Losowa pozycja kwadratu
     x = np.random.randint(0, image.size[0] - square_side)
     y = np.random.randint(0, image.size[1] - square_side)
     square_coords = [(x, y), (x + square_side, y + square_side)]
 
-    # Rysowanie białego kwadratu
     draw.rectangle(square_coords, fill=(255, 255, 255))
     
     return image
 
 if __name__ == '__main__':
-    # Załaduj dane z dysku
-    ds = load_from_disk(r'C:\vscodeProjects\WikiArt_Inpainting\WikiArt_damaged_100')
+    # Ładowanie danych
+    ds = load_from_disk(r'C:\vscodeProjects\WikiArt_Inpainting\dataset\WikiArt_damaged')
     
+    print('ds loaded')
+
     start = time()
-    images = ds['image']
+    num_images = len(ds)
+    batch_size = 5000
 
-    # Przetwarzanie obrazów: nakładanie uszkodzenia, konwersja na numpy i skalowanie do uint8
-    image_arrays = [np.array(generate_square_damage(img).convert("RGB"), dtype=np.uint8) for img in images]
+    sample_image = generate_square_damage(ds[0]['image'], square_side=32).convert("RGB")
+    image_shape = (3, sample_image.height, sample_image.width)
 
-    # Połącz listę numpy array w jedną tablicę (B x H x W x C) i zamień oś na (B x C x H x W)
-    image_array_stack = np.stack(image_arrays).transpose(0, 3, 1, 2)
+    print('sample image done')
 
-    # Zapisz dane numpy do pliku .h5 jako uint8
     with h5py.File('damaged_images_tensor_data.h5', 'w') as h5f:
-        h5f.create_dataset('image', data=image_array_stack, compression='gzip', compression_opts=9, dtype='uint8')
+        dataset = h5f.create_dataset('image', shape=(num_images, *image_shape), 
+                                     dtype='uint8', compression='gzip', compression_opts=9)
+
+        print('h5 created')
+
+        for i in tqdm(range(0, num_images, batch_size), desc='Batch', position=0):
+            # Pobranie batcha obrazów
+            batch_images = ds[i:i + batch_size]['image']
+
+            batch_data = [
+                np.array(generate_square_damage(img, square_side=32).convert("RGB"), dtype=np.uint8).transpose(2, 0, 1)
+                for img in tqdm(batch_images, desc='Batch progress', position=1, leave=False)
+            ]
+
+            dataset[i:i + len(batch_data)] = batch_data
 
     end = time()
-
     print(f'{end - start} seconds')
-    print("Obrazy z uszkodzeniami zostały zapisane do pliku damaged_images_tensor_data.h5 jako uint8")
+    print('Done')
